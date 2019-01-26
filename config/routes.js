1b23120a0b949027dfcd34738c42b87a03d6001e
Chris Roth <c.roth.lambda@gmail.com>
2019-01-26 12:15:46 -0600

    Sprint Challenge Authentication updates: added env file for secret and port, added imports to Routes, added jwt token generator to Routes, building first end point added Joi (for validation) and morgan (easier development on requests) as dependancies

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,34 @@
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig');
 
 const { authenticate } = require('../auth/authenticate');
-
+const jwtKey = process.env.JWT_KEY || 'It\'s a secret';
+const 
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+createToken=(user)=>{
+  const payload={
+    username: user.username,
+    id: user.id
+  };
+  const options={
+    expiresIn: '1.5h',
+    jwtid: toString(Date.now())
+  }
+  return jwt.sign(payload, jwtKey, options);
+
+}
+
 function register(req, res) {
-  // implement user registration
+  const endpoint = `http://localhost:${process.env.PORT}/api/register`;
+  const user = req.body;
+  db('users').post(user)
 }
 
 function login(req, res) {
